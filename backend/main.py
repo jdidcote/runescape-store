a7ea248403e04992ba607a06618da2e41a54f4f2
joshdidcote <joshua.didcote@outlook.com>
2023-01-22 20:39:53 +0000

    Always refetch local data

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,5 @@ import uvicorn
 from items.fetch_data import setup_data
 
 if __name__ == "__main__":
-    if not (Path(__file__).parent.parent / "data").exists():
-        setup_data()
+    setup_data()
     uvicorn.run("items.api:app", host="0.0.0.0", port=8000, reload=True)
